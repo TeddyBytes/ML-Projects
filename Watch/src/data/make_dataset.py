0df8488552f17da65c6c6be5768de5847276dae6
Matthew Audley <maudley@berkeley.edu>
2024-04-30 19:15:25 -0700

    Converted Time Features

--- Watch/src/data/make_dataset.py
+++ Watch/src/data/make_dataset.py
@@ -1,10 +1,13 @@
 import pandas as pd
+import numpy as np
 import regex as re
+import datetime
 from glob import glob
 
 # --------------------------------------------------------------
 # Read single CSV file
 # --------------------------------------------------------------
+
 one_acc_data = pd.read_csv(
     "../../data/raw/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv"
 )
@@ -19,7 +22,6 @@ one_gyro_data = pd.read_csv(
 
 data_path = "../../data/raw/*.csv"
 files = glob(data_path)
-# files
 
 # --------------------------------------------------------------
 # Extract features from filename
@@ -76,22 +78,29 @@ for file in files:
 
     # Build Respective DF's
     if "Accelerometer" in file:
-        df["set"] = acc_curr_set
+        df["Set"] = acc_curr_set
         acc_curr_set += 1
 
         all_acc_df = pd.concat([df, all_acc_df])
-    else:
-        df["set"] = gyr_curr_set
+    elif "Gyroscope":
+        df["Set"] = gyr_curr_set
         gyr_curr_set += 1
 
         all_gyr_df = pd.concat([df, all_gyr_df])
 
-    # print(exercise, participant, difficulty, rpe)
-all_acc_df
+
 # --------------------------------------------------------------
 # Working with datetimes
 # --------------------------------------------------------------
 
+acc_datetime = pd.to_datetime(all_acc_df["epoch (ms)"], unit="ms")
+gyrc_datetime = pd.to_datetime(all_gyr_df["epoch (ms)"], unit="ms")
+
+all_acc_df["time"] = acc_datetime
+all_gyr_df["time"] = gyrc_datetime
+
+all_acc_df.drop(["epoch (ms)", "time (01:00)", "elapsed (s)"], axis=1, inplace=True)
+all_gyr_df.drop(["epoch (ms)", "time (01:00)", "elapsed (s)"], axis=1, inplace=True)
 
 # --------------------------------------------------------------
 # Turn into function
